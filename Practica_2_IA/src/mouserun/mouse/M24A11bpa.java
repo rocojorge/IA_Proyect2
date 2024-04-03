4de881a31db291a89ead8b449a48e548227a3239
RUIZ1200 <aruimar2004@gmail.com>
2024-04-03 12:27:32 +0200

    Vergaso con el ganso

--- Practica_2_IA/src/mouserun/mouse/M24A11bpa.java
+++ Practica_2_IA/src/mouserun/mouse/M24A11bpa.java
@@ -1,190 +1,207 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-
 package mouserun.mouse;
 
-import mouserun.game.*;
+import mouserun.game.Cheese;
+import mouserun.game.Grid;
+import mouserun.game.Mouse;
 
 import java.util.*;
 
+
 /**
- *
- * @author gabri
+ * Esta clase representa un ratón explorador que intenta visitar todas las celdas posibles del laberinto.
+ * Utiliza dos mapas para almacenar las celdas visitadas: celdas_frecuentes y celdas_totales.
+ * El mapa celdas_frecuentes se reinicia cada vez que el ratón muere, mientras que el mapa celdas_totales se mantiene durante toda la ejecución.
+ * El ratón utiliza una pila de movimientos para volver atrás cuando se queda sin opciones, y hace return 0 cuando la pila está vacía.
  */
 public class M24A11bpa extends Mouse {
-    
+
+    // Constante que indica que no hay movimientos posibles
     private final Integer SIN_MOVIMIENTOS = 0;
-    
-    private class Pair<A, B> {
 
-        public A primero;
-        public B segundo;
+    // Mapa que almacena las celdas visitadas desde el último respawn
+    private HashMap<Integer, Grid> celdas_frecuentes;
+    // Mapa que almacena todas las celdas visitadas durante la ejecución
+    private HashMap<Integer, Grid> celdas_totales;
+    // Pila que almacena los movimientos realizados por el ratón
+    private LinkedList<Integer> movimientos;
 
-        public Pair() {
-        }
-
-        public Pair(A _first, B _second) {
-            primero = _first;
-            segundo = _second;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (!(o instanceof Pair)) {
-                return false;
-            }
-            Pair key = (Pair) o;
-            return primero == key.primero && segundo == key.segundo;
-        }
-
-        @Override
-        public int hashCode() {
-            if (primero instanceof Integer && segundo instanceof Integer) {
-                Integer result = (Integer) primero;
-                Integer sec = (Integer) segundo;
-                return result * 999 + sec;
-            }
-
-            return 0;
-        }
-
-        @Override
-        public String toString() {
-            return "X: " + primero + " Y: " + segundo;
-        }
+    /**
+     * Constructor de la clase Segunda_Prueba.
+     * Inicializa los atributos con valores vacíos y asigna el nombre "Explorador" al ratón.
+     */
+    public M24A11bpa(){
+        super("M24A11_Explorer");
+        this.movimientos=new LinkedList<>();
+        this.celdas_frecuentes=new HashMap<>();
+        this.celdas_totales=new HashMap<>();
+        
     }
 
     /**
-     * Almacena una posición(x,y) y las direcciones accesibles desde la misma.
-     * Esto último, solo será válido si el nodo está marcado como explored.
+     * Método que determina el siguiente movimiento del ratón en función de la celda actual.
+     * Añade la celda actual a los mapas de memoria y comprueba si hay movimientos posibles.
+     * Si los hay, devuelve uno de ellos al azar, evitando los que ya ha visitado.
+     * Si no los hay, devuelve el método sin_movimientos() que gestiona la pila de movimientos.
+     * @param currentGrid La celda actual del ratón
+     * @param cheese El queso actual
+     * @return Un entero que representa el movimiento elegido (Mouse.UP, Mouse.DOWN, Mouse.LEFT, Mouse.RIGHT o Mouse.BOMB)
      */
-    private class Nodo {
-
-        public int x,y;
-        public boolean up,down;
-        public boolean left;
-        public boolean right;
-        public boolean explored;
-        
-        //#1
-        public Nodo(int _x, int _y, boolean _up, boolean _down, boolean _left, boolean _right) {
-            x= _x;
-            y= _y;
-            up= _up;
-            down= _down;
-            left= _left;
-            right= _right;
-            explored=true;
-        }
-        //#2
-        public Nodo(Pair<Integer, Integer> pos, boolean _up, boolean _down, boolean _left, boolean _right) {
-            this(pos.primero, pos.segundo, _up, _down, _left, _right);
-        }
-        //#3
-        public Nodo(int _x, int _y){
-            x=_x;
-            y=_y;
-            explored = false;
-        }
-        //#4
-        public Nodo(Pair<Integer, Integer> pos) {
-            this(pos.primero, pos.segundo);
-        }
-
-        public Pair<Integer, Integer> getPos() {
-            return new Pair(x, y);
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (!(o instanceof Nodo)) {
-                return false;
-            }
-            Nodo node = (Nodo) o;
-            return x == node.x && y == node.y;
-        }
-
-        @Override
-        public int hashCode() {
-            return x * 999 + y;
-        }
-
-        @Override
-        public String toString() {
-            return "X: " + x + " Y: " + y;
-        }
-    }
-    
-    
-    private HashMap<Pair<Integer, Integer>, Nodo> Nodo_so =  new HashMap<>();
-    private HashMap<Pair<Integer,Integer>,Nodo> Nodos_corto;
-    private Nodo Nodo_del_principio;
-    private LinkedList<Integer> movimientos_devuelta;
-    private Queue<Pair<Integer,Integer>> Cola_ = new ArrayDeque<>();//Cola de nodos a visitar en una vuelta
-
-        
-    
-    public M24A11bpa(){
-        super("M24A11_BreadthExplorer");
-        this.Nodo_so = new HashMap<>();
-        this.Cola_=new ArrayDeque<>();
-    }
-    
+    @Override
     public int move(Grid currentGrid, Cheese cheese) {
-        //Primero me fijo en la cola y hago la cola
-        //Escribir aquí
-        if (Cola_.isEmpty()){return 0;//Vuelvo al inicio con la pila
-        }
-        else{
-            ArrayList<Integer> movimientos = posibles_movimientos(currentGrid);
-            
-            
-        }
-        //Evalúo mis alrededores y listo casillas.
-        
-        return 1;
+        agrega_celda(currentGrid);
+        Integer movimiento=posibles_movimientos(currentGrid);
+        if (!movimiento.equals(SIN_MOVIMIENTOS))
+            return movimiento;
+        else
+            return sin_movimientos();
     }
     
-    private ArrayList<Integer> posibles_movimientos(Grid celda_actual)
+    /**
+     * Método que devuelve una lista de los movimientos posibles desde una celda_actual.
+     * Comprueba si la celda_actual tiene salidas en las cuatro direcciones y las añade a una lista.
+     * Si la lista no está vacía, recorre sus elementos y elimina los que corresponden a celdas ya visitadas.
+     * @param celda_actual La celda actual del ratón
+     * @return Un entero que representa un movimiento posible al azar, o SIN_MOVIMIENTOS si no hay ninguno
+     */
+    private Integer posibles_movimientos(Grid celda_actual)
     {
         ArrayList<Integer> posibles = new ArrayList<>();
         if (celda_actual.canGoDown()) posibles.add(Mouse.DOWN);
         if (celda_actual.canGoUp()) posibles.add(Mouse.UP);
         if (celda_actual.canGoLeft()) posibles.add(Mouse.LEFT);
         if (celda_actual.canGoRight()) posibles.add(Mouse.RIGHT);
-        
-        return posibles; 
-            
-    }
-    
-    private void casillas_visitar(Grid celda_actual){
-        ArrayList<Integer> posibles;
-        posibles = posibles_movimientos(celda_actual);
-        
-        for(int i =0; i<posibles.size();i++){
-            if (posibles.get(i)==1){
-                Pair<Integer,Integer> root_up = new Pair(celda_actual.getX(),celda_actual.getY()+1);
-                Nodo celda_root_up= new Nodo(celda_actual.getX(),celda_actual.getY());
-                Nodo_so.putIfAbsent(root_up,celda_root_up);
+        if (!posibles.isEmpty()){
+            for (int i=0;i<posibles.size(); i++){
+                Integer movimiento_ret = posibles.get(i);
+                if (!celda_visitada(movimiento_ret, celda_actual)){
+                    movimientos.push(movimiento_inverso(movimiento_ret));
+                    return movimiento_ret;
+                }
             }
         }
-        
-        
-    }
-    public void respawned(){}
-    
-    public void newCheese(){
-        
+        return SIN_MOVIMIENTOS;
     }
     
+    /**
+     * Método que comprueba si una celda en una dirección determinada ha sido visitada o no.
+     * Calcula las coordenadas de la celda en la dirección indicada y busca su identificador_celda en el mapa celdas_frecuentes.
+     * @param direccion La dirección a comprobar (Mouse.UP, Mouse.DOWN, Mouse.LEFT o Mouse.RIGHT)
+     * @param celda La celda actual del ratón
+     * @return Un booleano que indica si la celda ha sido visitada (true) o no (false)
+     */
+    private Boolean celda_visitada(Integer direccion,Grid celda){
+        
+        int x=celda.getX();
+        int y=celda.getY();
+        switch (direccion)
+        {
+            case Mouse.UP:
+                y++;
+                break;
+            case Mouse.DOWN:
+                y--;
+                break;
+            case Mouse.LEFT:
+                x--;
+                break;
+            case Mouse.RIGHT:
+                x++;
+                break;
+        }
+        boolean to_ret=(celdas_frecuentes.get(identificador_celda(x,y)) == null);
+        return !(to_ret);
+    }
     
+    /**
+     * Método que devuelve el movimiento movimiento_inverso a uno dado.
+     * Utiliza un switch para asignar el valor correspondiente según la dirección.
+     * @param direccion La dirección a invertir (Mouse.UP, Mouse.DOWN, Mouse.LEFT o Mouse.RIGHT)
+     * @return Un entero que representa el movimiento movimiento_inverso
+     */
+    private Integer movimiento_inverso(Integer direccion){
+        
+        Integer movimiento_inverso = 0;
+
+        switch (direccion){
+            case Mouse.UP:
+                movimiento_inverso=Mouse.DOWN;
+                break;
+            case Mouse.LEFT:
+                movimiento_inverso=Mouse.RIGHT;
+                break;
+            case Mouse.RIGHT:
+                movimiento_inverso=Mouse.LEFT;
+                break;
+            case Mouse.DOWN:
+                movimiento_inverso=Mouse.UP;
+                break;
+        }
+        return movimiento_inverso;
+    }
+
+    /**
+     * Método que se ejecuta cuando el ratón muere y vuelve a aparecer en el laberinto.
+     * Reinicia el mapa celdas_frecuentes y la pila de movimientos, manteniendo el mapa celdas_totales.
+     */
+    @Override
+    public void respawned()
+        {
+        celdas_frecuentes=new HashMap<>();
+        movimientos=new LinkedList<>();
+    }
+
+    /**
+     * Método que se ejecuta cuando aparece un nuevo queso en el laberinto.
+     * Reinicia el mapa celdas_frecuentes y la pila de movimientos, manteniendo el mapa celdas_totales.
+     */
+    @Override
+    public void newCheese() {
+    }
+
+    /**
+     * Método que se ejecuta cuando el ratón no tiene movimientos posibles.
+     * Si la pila de movimientos no está vacía, devuelve el último movimiento inverso almacenado.
+     * Si la pila está vacía, reinicia el mapa celdas_frecuentes y devuelve 0.
+     * @return Un entero que representa el movimiento elegido (Mouse.UP, Mouse.DOWN, Mouse.LEFT, Mouse.RIGHT o Mouse.BOMB)
+     */
+    private Integer sin_movimientos(){
+        if ( !movimientos.isEmpty() ){
+            return movimientos.pop();
+        } else 
+        {
+            celdas_frecuentes = new HashMap<>();
+        }
+        return 0;
+    }
+
+    /**
+     * Método que añade una celda a los mapas de memoria.
+     * Genera una identificador_celda única para la celda a partir de sus coordenadas x e y, y la añade al mapa celdas_frecuentes si no está presente.
+     * Si la celda no está en el mapa celdas_totales, la añade y llama al método incExploredGrids() para incrementar el contador de celdas exploradas.
+     * @param celda La celda a añadir
+     */
+    private void agrega_celda( Grid celda ){
+        
+        int x=celda.getX();
+        int y=celda.getY();
+
+        celdas_frecuentes.putIfAbsent( identificador_celda(x, y), celda );
+
+        if (celdas_totales.get(identificador_celda(x, y)) == null) {
+            celdas_totales.put(identificador_celda(x, y), celda);
+            incExploredGrids();
+        }
+    }
+
+    /**
+     * Método que genera una identificador_celda única para una celda a partir de sus coordenadas x e y.
+     * Multiplica la coordenada x por 99999 y le suma la coordenada y, asumiendo que ambas son positivas y menores que 100.
+     * @param x La coordenada x de la celda
+     * @param y La coordenada y de la celda
+     * @return Un entero que representa la identificador_celda de la celda
+     */
+    private Integer identificador_celda( int x, int y ){
+        int to_ret= (x*99999+y);
+        return to_ret;
+    }    
 }
